eb6bef7bb64aa46083c3f15debac8d5dbbe2a590
EC2 Default User <ec2-user@ip-172-31-87-7.ec2.internal>
2019-10-13 01:50:05 +0000

    init docker scripts

---
+++ dependency.sh
@@ -0,0 +1,5 @@
+#sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux  docker-engine
+#sudo amazon-linux-extras install docker
+#sudo pip3 install docker-compose
+
+
